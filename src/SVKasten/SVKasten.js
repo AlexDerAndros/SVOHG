93e54603d2f3eba240155b12eb0a928d7fdb5bbb
AlexDerAndros <126338368+AlexDerAndros@users.noreply.github.com>
2024-04-29 00:24:21 +0200

    Update SVKasten.js

--- src/SVKasten/SVKasten.js
+++ src/SVKasten/SVKasten.js
@@ -2,7 +2,21 @@ import "./SVKasten.css";
 export default function SVKasten() {
    return (
      <>
-       SV Kasten
+       <div className="head">
+         SV Kasten 
+       </div>
+       <div className="textCon">
+          <div className="text">
+            Wilkommen beim SV Kasten! Hier könnt ihr eure Wünsche, Beschwerden und so weiter
+            bei uns einrreichen, damit wir diese dann, wenn es möglich ist, zu erfüllen, um die Schule zu 
+            einem besseren Ort zu machen.
+          </div>
+       </div>
+       <input type="text" placeholder="Was wollt ihr uns mitteilen?" className="inSVKasten"/>
+       <br/>
+       <button className="btnSVKasten">
+         Senden
+       </button>
      </>
    );
 }
