9ba8929d3ec0aebcacc5a55c89b566de5729bde1
Marcodoro <118012046+Marcodoro@users.noreply.github.com>
2024-05-03 19:52:01 +0200

    LOGIN UPDATE design
    Updated Layout and UI and overall design of login form.

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -2,7 +2,23 @@ import "./Login.css";
 export default function Login() {
     return (
       <>
-        Login
+        <div className="main">
+          <div className="square">
+            <div className="con_3">
+                <div className="title_login">
+                    Login
+                </div>
+            </div>
+            <div className="inputs_5">
+              <label for="email">Email</label>
+              <input type="text" placeholder="Email or Phone" id="username" />
+
+              <label for="password">Password</label>
+              <input type="password" placeholder="Password" id="password" />
+              <button className="bt">Log In</button>
+            </div>
+          </div>
+        </div>
       </>
     );
 
