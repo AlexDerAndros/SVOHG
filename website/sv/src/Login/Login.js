c5a97c84b8f58b5149e0b8b318c23ffb0c09f245
AlexDerAndros <ntrikos@gmx.de>
2024-05-12 00:15:35 +0200

    login + buck fix

--- website/sv/src/Login/Login.js
+++ website/sv/src/Login/Login.js
@@ -1,26 +1,79 @@
 import "./Login.css";
+import Cookies from 'js-cookie';
+import { useState } from "react";
 export default function Login() {
-    return (
-      <>
-        <div className="main">
-          <div className="square">
-            <div className="con_3">
-                <div className="title_login">
-                    Login
-                </div>
-            </div>
-            <br />
-            <div className="inputs_5">
-              <label for="email">E-Mail</label>
-              <input type="text" placeholder="E-Mail oder Telefonnummer" id="username" />
+  const[log, setLog] = useState(false);
+  let user;
+  let pa;
+  const[username, setUsername] = useState('');
+  const[password, setPassword] = useState(''); 
+  const USAdminSV = "SVHost";
+  const PAAdminSV = "Test";
+  const logBtn = () => {
+    if ( username == USAdminSV && password == PAAdminSV ) {
+      setLog(true);
+      Cookies.set("user", user, {expires: 7});
+      Cookies.set("password", pa, {expires: 7});
+      alert("Login erfolgreich")
+    }
+    else {
+      setLog(false);
+      alert("Login fehlgeschlagen");
+    }
+  };
+  
+  return (
+   <div className="main">
+      { log ? (
+        <LoggedIn/>
+      ) : (
+        <LoggingIn logBtn={logBtn} 
+                    password={password} 
+                    setPassword={setPassword} 
+                    username={username} 
+                    setUsername={setUsername}/>
+      )}
+   </div>
+  );
 
-              <label for="password">Passwort</label>
-              <input type="password" placeholder="Passwort" id="password" />
-              <button className="bt">Log In</button>
-            </div>
+}
+function LoggingIn({logBtn, password, setPassword, username, setUsername}) {
+  return (
+    <>
+      <div className="main">
+        <div className="square">
+          <div className="con_3">
+              <div className="title_login">
+                  Login
+              </div>
+          </div>
+          <br />
+          <div className="inputs_5">
+            <label for="email">E-Mail</label>
+            <input type="text" 
+                   placeholder="E-Mail oder Nutzername..." 
+                   id="username" 
+                   value={username} 
+                   onChange={(e) => setUsername(e.target.value)} />
+
+            <label for="password">Passwort</label>
+            <input type="password" 
+                   placeholder="Passwort" 
+                   id="password" 
+                   value={password} 
+                   onChange={(e) => setPassword(e.target.value)}/>
+            <button className="bt" onClick={logBtn}>Log In</button>
           </div>
         </div>
-      </>
-    );
-
+      </div>
+    </>
+  );
+}
+function LoggedIn() {
+  let us = Cookies.get("user");
+  return (
+    <>
+     Wilkommen {us}!
+    </>
+  );
 }
